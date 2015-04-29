3702ae169fb2f51e2d2eeccdf80d8e78ce5c45c7
DQNEO <dqneoo@gmail.com>
2015-04-29 19:18:34 +0900

    integrate

--- app/action/Index.php
+++ app/action/Index.php
@@ -8,66 +8,6 @@
  */
 
 /**
- *  Index form implementation
- *
- *  @author    {$author}
- *  @access    public
- *  @package   Wiki
- */
-
-class Wiki_Form_Index extends Wiki_ActionForm
-{
-    /**
-     *  @access   protected
-     *
-     *  @var      array   form definition.
-     */
-    public $form = array(
-       /*
-        *  TODO: Write form definition which this action uses.
-        *  @see http://ethna.jp/ethna-document-dev_guide-form.html
-        *
-        *  Example(You can omit all elements except for "type" one) :
-        *
-        *  'sample' => array(
-        *      // Form definition
-        *      'type'        => VAR_TYPE_INT,    // Input type
-        *      'form_type'   => FORM_TYPE_TEXT,  // Form type
-        *      'name'        => 'Sample',        // Display name
-        *
-        *      //  Validator (executes Validator by written order.)
-        *      'required'    => true,            // Required Option(true/false)
-        *      'min'         => null,            // Minimum value
-        *      'max'         => null,            // Maximum value
-        *      'regexp'      => null,            // String by Regexp
-        *      'mbregexp'    => null,            // Multibype string by Regexp
-        *      'mbregexp_encoding' => 'UTF-8',   // Matching encoding when using mbregexp
-        *
-        *      //  Filter
-        *      'filter'      => 'sample',        // Optional Input filter to convert input
-        *      'custom'      => null,            // Optional method name which
-        *                                        // is defined in this(parent) class.
-        *  ),
-        */
-    );
-
-    /**
-     *  Form input value convert filter : sample
-     *
-     *  @access protected
-     *  @param  mixed   $value  Form Input Value
-     *  @return mixed           Converted result.
-     */
-    /*
-    protected function _filter_sample($value)
-    {
-        //  convert to upper case.
-        return strtoupper($value);
-    }
-    */
-}
-
-/**
  *  Index action implementation.
  *
  *  @author     {$author}
@@ -76,24 +16,6 @@ class Wiki_Form_Index extends Wiki_ActionForm
  */
 class Wiki_Action_Index extends Wiki_ActionClass
 {
-    /**
-     *  preprocess Index action.
-     *
-     *  @access    public
-     *  @return    string  Forward name (null if no errors.)
-     */
-    public function prepare()
-    {
-        /**
-        if ($this->af->validate() > 0) {
-            // forward to error view (this is sample)
-            return 'error';
-        }
-        $sample = $this->af->get('sample');
-        */
-        return null;
-    }
-
     /**
      *  Index action implementation.
      *
@@ -105,4 +27,3 @@ class Wiki_Action_Index extends Wiki_ActionClass
         return 'index';
     }
 }
-
